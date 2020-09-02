7b3ee70407c29261d969fcc9144f29232c8a6d0b
Daniel Muino <dmuino@netflix.com>
2020-09-02 09:26:17 -0700

    Add CPU scaling frequency metrics
    We add three distribution summary to track the min, max, and current
    scaling frequency for all cores.
    
    This was requested internally to be able to track the % of time that we
    are getting above the minimum CPU frequency per period, and how close we
    are to the maximum.

---
+++ lib/cpufreq.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <spectator/registry.h>
+
+namespace atlasagent {
+class CpuFreq {
+ public:
+  explicit CpuFreq(spectator::Registry* registry,
+                   std::string path_prefix = "/sys/devices/system/cpu") noexcept;
+
+  void Stats() noexcept;
+
+ private:
+  spectator::Registry* registry_;
+  std::string path_prefix_;
+  bool enabled_;
+
+  std::shared_ptr<spectator::DistributionSummary> min_ds_;
+  std::shared_ptr<spectator::DistributionSummary> max_ds_;
+  std::shared_ptr<spectator::DistributionSummary> cur_ds_;
+};
+}  // namespace atlasagent
