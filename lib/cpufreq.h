3758eba089b834066902f83fd3355db7af9124aa
Daniel Muino <dmuino@netflix.com>
2020-09-04 15:09:43 -0700

    Fix path for cpu frequency files
    The default path which is used during normal operations was wrong
    (mainly due to an update to make the test resources easy to find)

--- lib/cpufreq.h
+++ lib/cpufreq.h
@@ -6,7 +6,7 @@ namespace atlasagent {
 class CpuFreq {
  public:
   explicit CpuFreq(spectator::Registry* registry,
-                   std::string path_prefix = "/sys/devices/system/cpu") noexcept;
+                   std::string path_prefix = "/sys/devices/system/cpu/cpufreq") noexcept;
 
   void Stats() noexcept;
 
