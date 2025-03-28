43256807465d77d9629c3d4ebf72167e8d3316ff
Everett Badeaux <everettc1810@gmail.com>
2025-03-28 13:22:40 -0500

    Fix broken build

--- conanfile.py
+++ conanfile.py
@@ -17,7 +17,9 @@ class AtlasSystemAgentConan(ConanFile):
         "openssl/3.3.2",
         "rapidjson/cci.20230929",
         "sdbus-cpp/2.0.0",
+        "dbus/1.15.8",
         "spdlog/1.15.0",
+        "zstd/1.5.5",
         "zlib/1.3.1",
     )
     tool_requires = ()
