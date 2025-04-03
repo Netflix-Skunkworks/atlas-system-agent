eb46c81ab2ea8a9ab9d7309699aba70a81ab4702
Everett Badeaux <everettc1810@gmail.com>
2025-04-03 14:13:24 +0000

    fix build

--- conanfile.py
+++ conanfile.py
@@ -18,11 +18,14 @@ class AtlasSystemAgentConan(ConanFile):
         "rapidjson/cci.20230929",
         "sdbus-cpp/2.0.0",
         "spdlog/1.15.0",
-        "zstd/1.5.5",
         "zlib/1.3.1",
     )
     tool_requires = ()
     generators = "CMakeDeps", "CMakeToolchain"
+
+    def requirements(self):
+        # To Do: remove this when SystemD updates package for zstd
+        self.requires("zstd/1.5.7", override=True)
 
     def configure(self):
         self.options["libcurl"].with_c_ares = True
