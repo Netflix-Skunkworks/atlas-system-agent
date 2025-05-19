5c6d5556ac7febaca1465b0eae9b18c382baec22
Everett Badeaux <everettc1810@gmail.com>
2025-05-19 11:02:05 -0500

    Remove Header Only Monotonic Timer (#152)
    * Clean Header File
    
    * Sort lib includes

---
+++ lib/monotonic_timer/src/monotonic_timer.cpp
@@ -0,0 +1,32 @@
+
+#include "monotonic_timer.h"
+#include <lib/tagging/src/tagging_registry.h>
+
+
+namespace atlasagent {
+    
+template class MonotonicTimer<atlasagent::TaggingRegistry>;
+template class MonotonicTimer<spectator::TestRegistry>;
+
+template <typename Reg>
+MonotonicTimer<Reg>::MonotonicTimer(Reg* registry, const spectator::Id& id)
+    : count_{registry->GetCounter(id.WithStat("count"))},
+      total_time_{registry->GetCounter(id.WithStat("totalTime"))} {}
+
+template <typename Reg>
+void MonotonicTimer<Reg>::update(absl::Duration monotonic_time, int64_t monotonic_count) {
+  if (prev_count > 0) {
+    auto delta_count = monotonic_count - prev_count;
+    if (delta_count > 0) {
+      auto seconds = absl::ToDoubleSeconds(monotonic_time - prev_time);
+      if (seconds >= 0) {
+        total_time_->Add(seconds);
+        count_->Add(delta_count);
+      }
+    }
+  }
+  prev_time = monotonic_time;
+  prev_count = monotonic_count;
+}
+
+}  // namespace atlasagent
