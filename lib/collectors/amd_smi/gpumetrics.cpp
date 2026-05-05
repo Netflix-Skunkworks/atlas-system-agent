89a39b1081b57a9429909ed508b07e36e255e942
Everett Badeaux <ebadeaux@netflix.com>
2026-05-05 15:57:50 -0500

    Fix compile error

--- lib/collectors/amd_smi/gpumetrics.cpp
+++ lib/collectors/amd_smi/gpumetrics.cpp
@@ -55,9 +55,9 @@ void GpuMetricsAMD::GetMemoryMetrics(unsigned int i, amdsmi_processor_handle han
     {
         return;
     }
-    detail::gauge(registry_, "gpu.usedMemory", i).Set(memory.used);
-    detail::gauge(registry_, "gpu.freeMemory", i).Set(memory.free);
-    detail::gauge(registry_, "gpu.totalMemory", i).Set(memory.total);
+    amd_smi_detail::gauge(registry_, "gpu.usedMemory", i).Set(memory.used);
+    amd_smi_detail::gauge(registry_, "gpu.freeMemory", i).Set(memory.free);
+    amd_smi_detail::gauge(registry_, "gpu.totalMemory", i).Set(memory.total);
 }
 
 void GpuMetricsAMD::GetActivityMetrics(unsigned int i, amdsmi_processor_handle handle) noexcept
@@ -67,8 +67,8 @@ void GpuMetricsAMD::GetActivityMetrics(unsigned int i, amdsmi_processor_handle h
     {
         return;
     }
-    detail::gauge(registry_, "gpu.utilization", i).Set(activity.gfx);
-    detail::gauge(registry_, "gpu.memoryActivity", i).Set(activity.umc);
+    amd_smi_detail::gauge(registry_, "gpu.utilization", i).Set(activity.gfx);
+    amd_smi_detail::gauge(registry_, "gpu.memoryActivity", i).Set(activity.umc);
 }
 
 void GpuMetricsAMD::GetClockMetrics(unsigned int i, amdsmi_processor_handle handle) noexcept
@@ -78,8 +78,8 @@ void GpuMetricsAMD::GetClockMetrics(unsigned int i, amdsmi_processor_handle hand
     {
         return;
     }
-    detail::gauge(registry_, "gpu.clockFrequency", i, "gpu").Set(clocks.gfx_mhz);
-    detail::gauge(registry_, "gpu.clockFrequency", i, "memory").Set(clocks.mem_mhz);
+    amd_smi_detail::gauge(registry_, "gpu.clockFrequency", i, "gpu").Set(clocks.gfx_mhz);
+    amd_smi_detail::gauge(registry_, "gpu.clockFrequency", i, "memory").Set(clocks.mem_mhz);
 }
 
 void GpuMetricsAMD::GetTemperatureMetric(amdsmi_processor_handle handle) noexcept
@@ -101,7 +101,7 @@ void GpuMetricsAMD::GetPowerMetric(unsigned int i, amdsmi_processor_handle handl
     {
         return;
     }
-    detail::gauge(registry_, "gpu.power", i).Set(power);
+    amd_smi_detail::gauge(registry_, "gpu.power", i).Set(power);
 }
 
 void GpuMetricsAMD::GetPcieMetrics(unsigned int i, amdsmi_processor_handle handle) noexcept
@@ -111,9 +111,9 @@ void GpuMetricsAMD::GetPcieMetrics(unsigned int i, amdsmi_processor_handle handl
     {
         return;
     }
-    detail::counter(registry_, "gpu.amd.pcie.bytes", i, "out")
+    amd_smi_detail::counter(registry_, "gpu.amd.pcie.bytes", i, "out")
         .Increment(pcie.out_bytes_per_sec * AmdSmiConstants::BytesConversion);
-    detail::counter(registry_, "gpu.amd.pcie.bytes", i, "in")
+    amd_smi_detail::counter(registry_, "gpu.amd.pcie.bytes", i, "in")
         .Increment(pcie.in_bytes_per_sec * AmdSmiConstants::BytesConversion);
 }
 
@@ -124,9 +124,9 @@ void GpuMetricsAMD::GetXgmiMetrics(unsigned int i, amdsmi_processor_handle handl
     {
         return;
     }
-    detail::counter(registry_, "gpu.amd.xgmi.bytes", i, "out")
+    amd_smi_detail::counter(registry_, "gpu.amd.xgmi.bytes", i, "out")
         .Increment(xgmi.out_bytes_per_sec * AmdSmiConstants::BytesConversion);
-    detail::counter(registry_, "gpu.amd.xgmi.bytes", i, "in")
+    amd_smi_detail::counter(registry_, "gpu.amd.xgmi.bytes", i, "in")
         .Increment(xgmi.in_bytes_per_sec * AmdSmiConstants::BytesConversion);
 }
 
